f4962e0cfd6ece30524ca1032452b18598109f5f
giameconcept-star <giameconcept@gmail.com>
2025-12-31 19:12:06 +0100

    Practice: finding the largest number in an array

---
+++ largest number.js
@@ -0,0 +1,9 @@
+let numbers = [3, 7, 2, 9, 5];
+let largest = numbers[0];
+for (let i=1 i< numbers.length; i++) { 
+  if (numbers[i]> largest) {
+    largest = numbers[i];
+  }
+}
+
+console.log(largest);
